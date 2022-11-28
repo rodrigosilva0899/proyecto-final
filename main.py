3f8907dfcc5630766ab0497895c615e6beb98f1f
Rodrigo Silva <rodrigosilva0899@gmail.com>
2022-11-28 19:14:19 +0100

    Actualizo main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 print("hello world")
 print('hola mundo')
+
+# para agregar un cambio es:
+# 1) git add main.py
+# 2) git status para chequear
+# 3) git commin -m "mensaje"
+# 4) git push origin main
+
