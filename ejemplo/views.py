39e001705a26b43be8622addff9fc1bc3013ca46
Rodrigo Silva <rodrigosilva0899@gmail.com>
2022-12-04 23:58:06 +0000

    agrego clase 19

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -17,3 +17,23 @@ def sumar(request, a, b):
     "ejemplo/sumar.html",
     {"a": a, "b": b, "resultado": a + b})
 
+
+def buscar(request):
+    lista_de_nombre = ['Rodrigo', 'Tomas', 'Nicolas', 'Santiago', 'Silvina']
+    query = request.GET['q']
+
+    if query in lista_de_nombre:
+        indice_de_resultado = lista_de_nombre.index(query)
+        resultado = lista_de_nombre[indice_de_resultado]
+
+    else:
+        resultado = 'no hay match'
+
+    return render(request, 'ejemplo/buscar.html', {'resultado': resultado})
+
+
+
+
+
+
+
